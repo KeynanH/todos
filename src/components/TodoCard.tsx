cfb80a494a8416d15db47ac79699a0b09cce3861
keynanhemmings <96178536+keynanhemmings@users.noreply.github.com>
2026-09-03 22:58:44 +0100

    installed calendar library and successfully rendered TodoList

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,6 +1,16 @@
-export default function TodoCard({}){
+import { Todo } from "@/types/todo"
+
+interface TodoCardProps {
+    tasks: Todo
+}
+
+export default function TodoCard({tasks}: TodoCardProps){
 
     return(
-<></>
+        <div className="flex items-center justify-betweem p-4 rounded-xl border bg-white transition-all duration-200">
+            {tasks.title}
+            {tasks.description}
+            {tasks.completed}
+        </div>
     )
 } 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,26 @@
-export default function TodoList(){
+import TodoCard from "./TodoCard"
+import { Todo } from "@/types/todo"
+
+interface TodoListProps{
+    tasks: Todo[]
+}
+export default function TodoList({tasks} : TodoListProps){
+
+    if (tasks.length === 0){
+        return(
+            <div className="text-center py-12 px-4 rounded-xl border border-dashed border-slate-200">
+                <p>No tasks found, You are all caught up!</p>
+            </div>
+        )
+    }
     return(
-        <></>
+        <div>
+            {tasks.map((task) => (
+                <TodoCard
+                    key={task.id}
+                    tasks={task}
+                />
+            ))}
+        </div>
     )
 }
