d292419ccdadcf9219e0c863c81c0a5d18207e23
keynanhemmings <96178536+keynanhemmings@users.noreply.github.com>
2026-09-04 06:43:04 +0100

    remove footer and move sign out to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css"
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,17 @@
+'use client'
+import { signOut } from "next-auth/react";
+
 export default function Header(){
     return(
-        <></>
+            <div className="flex flex-col py-6 px-4 w-full items-end justify-center bg-white">
+                <div>
+                    <button
+                    onClick={() => signOut({ callbackUrl: "/login" })}
+                    className="rounded-md bg-red-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 transition-colors"
+                    >
+                    Sign Out
+                    </button>
+                </div>
+            </div>
     )
 }
