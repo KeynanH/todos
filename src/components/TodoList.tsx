6091525a4a8858ffde35a54fb6da2977408970b8
keynanhemmings <96178536+keynanhemmings@users.noreply.github.com>
2026-09-04 17:24:35 +0100

    added crud operation to todo cards

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,33 @@
-import TodoCard from "./TodoCard"
-import { Todo } from "@/types/todo"
+'use client'
+import { Todo } from "@/types/todo";
+import { useTodos } from "@/hooks/useTodos";
+import TodoCard from "./TodoCard";
 
-interface TodoListProps{
-    tasks: Todo[]
+interface TodoListProps {
+  tasks: Todo[];
 }
-export default function TodoList({tasks} : TodoListProps){
 
-    if (tasks.length === 0){
-        return(
-            <div className="text-center py-12 px-4 rounded-xl border border-dashed border-slate-200">
-                <p className="text-black">No tasks found, You are all caught up!</p>
-            </div>
-        )
-    }
-    return(
-        <div>
-            {tasks.map((task) => (
-                <TodoCard
-                    key={task.id}
-                    tasks={task}
-                />
-            ))}
-        </div>
-    )
+export default function TodoList({ tasks }: TodoListProps) {
+  const { toggleTodoStatus, removeTodo } = useTodos();
+
+  if (tasks.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center p-8 text-center rounded-xl bg-gray-50 border border-dashed border-gray-300">
+        <p className="text-sm font-medium text-gray-500">No tasks found. click "Add todo" to add some</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-3 w-full max-h-[500px] overflow-y-auto pr-2">
+      {tasks.map((item) => (
+        <TodoCard
+          key={item.id}
+          task={item}
+          onToggleStatus={toggleTodoStatus}
+          onDelete={removeTodo}
+        />
+      ))}
+    </div>
+  );
 }
