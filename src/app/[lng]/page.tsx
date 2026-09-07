11c30833084ba12ad1ce7b602258fd58c15b1c58
keynanhemmings <96178536+keynanhemmings@users.noreply.github.com>
2026-09-07 03:55:46 +0100

    pushed to be added to the run time fix deployment

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -4,10 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import TodoDashboard from "@/components/TodoDashboard";
-import en from "../../locales/en.json"
-import de from "../../locales/de.json"
 
-const translations: Record<string, typeof en> = { en, de };
 
 interface HomeProps {
   params: Promise<{ lng: string}>
@@ -16,12 +13,13 @@ interface HomeProps {
 export default async function Home({params}: HomeProps) {
   
   const { lng } = await params;
-  const t = translations[lng] || translations['en'];
+
 
   const session = await getServerSession(authOptions)
     if (!session) {
     redirect("/login");
   }
   
-  return <TodoDashboard session={session} />
+  return <TodoDashboard session={session} lng={lng} />
+
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -29,7 +29,7 @@ export default function Header({lng} : HeaderProps){
                     onClick={() => signOut({ callbackUrl: "/login" })}
                     className="rounded-md bg-red-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 transition-colors"
                     >
-                    Sign Out
+                    {t("signout")}
                     </button>
                 </div>
             </div>
