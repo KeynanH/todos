1485591eda581e2482dafc15fe9c61587249142e
keynanhemmings <96178536+keynanhemmings@users.noreply.github.com>
2026-09-07 02:16:42 +0100

    localised login and register pages

--- src/app/[lng]/login/page.tsx
+++ src/app/[lng]/login/page.tsx
@@ -2,70 +2,53 @@
 
 import '@/i18n'
 import { signIn } from "next-auth/react";
-import { SyntheticEvent, use, useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import Link from "next/link";
+import { use, useEffect, useState } from "react";
+import { useTranslation } from 'react-i18next';
 
-interface RegisterPageProps{
-  params: Promise<{ lng:string}>
-
+interface LoginPageProps {
+  params: Promise<{ lng: string}>
 }
 
-export default function RegisterPage( {params}: RegisterPageProps) {
-    
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
+export default function LoginPage({ params }: LoginPageProps){
 
-  const { lng } = use(params);
-  const {t, i18n} = useTranslation()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
 
-  useEffect(() => {
-    if(lng && i18n.language !== lng){
-      i18n.changeLanguage(lng)
-    }
-  },[lng, i18n])
+    const { lng } = use(params);
+    const {t, i18n} = useTranslation()
 
-  const handleSubmit = async (e: SyntheticEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setError("")
-    setIsLoading(true)
+    useEffect(() => {
+      if(lng && i18n.language !== lng){
+        i18n.changeLanguage(lng)
+      }
+    },[lng, i18n])
 
-    try {
-        const res = await fetch("/api/register", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({email, password})
+    const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setError("")
+
+        const result = await signIn("credentials", {
+            redirect: true,
+            callbackUrl: "/",
+            email,
+            password
         })
 
-        const data = await res.json();
-
-        if (!res.ok) {
-            setError(data.error || "Something went wrong.");
-            setIsLoading(false);
-            return;
+        if (result?.error) {
+        setError(t('loginerror'));
         }
 
-        await signIn("credentials", {
-            redirect: true,
-            callbackUrl: "/", // Instantly brings them to your protected main page.tsx
-            email,
-            password,
-        });
-    } catch (error) {
-        setError(t('failedconnection'));
-        return
-    } finally{
-        setIsLoading(false);
     }
-  }
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-200 px-4">
+
+    return(
+        <div className="flex min-h-screen items-center justify-center bg-gray-200 px-4">
       <div className="w-full max-w-md space-y-6 rounded-xl bg-white p-8 shadow-md">
         <h2 className="text-center text-2xl font-bold tracking-tight text-black ">
-              {t('createaccount')}
+          {t('signintitle')}
         </h2>
-
+        
         {error && (
           <div className="rounded-md bg-red-50 p-3 text-sm text-red-600">
             {error}
@@ -83,7 +66,6 @@ export default function RegisterPage( {params}: RegisterPageProps) {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              disabled={isLoading}
               className="mt-1 block w-full rounded-md border border-gray-300 bg-gray-50 px-3 py-2 text-black placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm "
             />
           </div>
@@ -98,20 +80,25 @@ export default function RegisterPage( {params}: RegisterPageProps) {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              disabled={isLoading}
               className="mt-1 block w-full rounded-md border bg-gray-50 border-gray-300 px-3 py-2 text-black placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
             />
           </div>
 
           <button
             type="submit"
-            disabled={isLoading}
             className="flex w-full justify-center rounded-md bg-black px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            {isLoading ?  t('registering') : t("register")}
+            {t('signin')}
           </button>
         </form>
+
+        <p className="text-center text-sm text-gray-600 dark:text-gray-400">
+          {t('donthaveaccount')}{" "}
+          <Link href="/register" className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-indigo-400">
+            {t('registerhere')}
+          </Link>
+        </p>
       </div>
     </div>
-  )
+    )
 }
